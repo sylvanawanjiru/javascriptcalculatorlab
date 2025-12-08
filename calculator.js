d16875113f03af25ec4612d3a16435f1e424bfcb
sylvanawanjiru <sylvanawanjru@gmail.com>
2025-12-08 08:49:43 +0300

    first commit

---
+++ calculator.js
@@ -0,0 +1,17 @@
+// calculator.js
+// 1) Create an empty array to store calculation history 
+const calculationHistory = [];
+
+// 2) Function to add a calculation in history
+function saveToHistory(number1, number2 operaator, result) {
+    number1: number1,
+    number2: number2,
+    operator: operator,
+    result: result,
+
+    // 3) store the date and time 
+    //4) add the calculation to the history array
+
+};
+
+
